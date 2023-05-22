8cc78b8ad5758b6aee2b3b97e0749092c469b6f1
Lapindrome <shadowghostrider31@gmail.com>
2023-05-22 09:44:14 +0200

    Modif main.py

--- main.py
+++ main.py
@@ -4,8 +4,34 @@ import time
 
 start_time = time.time_ns()
 
+
+dictLegs = {
+    'Leg_1':1,
+    'Leg_2':2,
+    'Leg_3':3,
+    'Leg_4':4,
+    'Leg_5':5,
+    'Leg_6':6
+}
+
+
+
+def verificationMessage(message):
+        if( len(message["motors"]) == 0):
+            print("Aucun moteur renseigné")
+            return -1
+        
+        if( len(message["legs"]) == 0):
+            print("Aucune patte renseignée")
+            return -2
+        
+        return 0
+
+
+
 def robot_exec():
     message = transfer_queue.get()
+
     while True:
         if not message["loop"]:
             print("wait until loop starts")
@@ -17,11 +43,18 @@ def robot_exec():
             return
         print("doing something with", message)
 
+        if( not verificationMessage(message) ):
+            print("Movement robot")
+
+            for legs in dictLegs:
+                print(legs)
+
+
 
 # create the application
 app = App()
 
-# method calls to the window manager class
+# method calls to the window manager class<
 app.master.title("Hexapod")
 app.master.maxsize(1000, 400)
 
